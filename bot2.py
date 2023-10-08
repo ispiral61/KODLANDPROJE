149a08da6c5a6400c52c3620a20739d5818616cf
ed <145282976+ispiral61@users.noreply.github.com>
2023-10-08 20:36:19 +0300

    Create bot2.py

---
+++ bot2.py
@@ -0,0 +1,28 @@
+import discord
+from bot_mantik import gen_pass
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send(gen_pass(20))
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("YOUR TOKEN")
